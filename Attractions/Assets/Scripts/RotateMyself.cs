8f2dce6b0251613fdee9d4b154e72b7d995e51e5
daniel38530324 <89857241+daniel38530324@users.noreply.github.com>
2021-11-14 23:21:29 +0800

    世界景點導覽編輯檔

---
+++ Attractions/Assets/Scripts/RotateMyself.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateMyself : MonoBehaviour
+{
+    public float Rx, Ry, Rz;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Rx * Time.deltaTime, Ry * Time.deltaTime, Rz * Time.deltaTime);
+    }
+}
---
+++ Attractions/Assets/Scripts/model.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class model : MonoBehaviour
+{
+    public GameObject[] cube;
+    public GameObject[] models;
+    public GameObject[] ui;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(cube[0].GetComponent<MeshRenderer>().enabled == true)
+        {
+            models[0].SetActive(true);
+            ui[0].SetActive(true);
+        }
+        else if (cube[0].GetComponent<MeshRenderer>().enabled == false)
+        {
+            models[0].SetActive(false);
+            ui[0].SetActive(false);
+        }
+
+        if (cube[1].GetComponent<MeshRenderer>().enabled == true)
+        {
+            models[1].SetActive(true);
+            ui[1].SetActive(true);
+        }
+        else if (cube[1].GetComponent<MeshRenderer>().enabled == false)
+        {
+            models[1].SetActive(false);
+            ui[1].SetActive(false);
+        }
+
+        if (cube[2].GetComponent<MeshRenderer>().enabled == true)
+        {
+            models[2].SetActive(true);
+            ui[2].SetActive(true);
+        }
+        else if (cube[2].GetComponent<MeshRenderer>().enabled == false)
+        {
+            models[2].SetActive(false);
+            ui[2].SetActive(false);
+        }
+
+        if (cube[3].GetComponent<MeshRenderer>().enabled == true)
+        {
+            models[3].SetActive(true);
+            ui[3].SetActive(true);
+        }
+        else if (cube[3].GetComponent<MeshRenderer>().enabled == false)
+        {
+            models[3].SetActive(false);
+            ui[3].SetActive(false);
+        }
+    }
+}
